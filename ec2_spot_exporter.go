c3867b5418658b3ab509a9e5a23aa0ab73e392a1
Patrick Cadelina <patrickcadelina@yahoo.com>
2020-02-18 09:22:22 +0800

    Add `region` as label

--- ec2_spot_exporter.go
+++ ec2_spot_exporter.go
@@ -16,6 +16,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -63,163 +64,163 @@ func NewCollector() *Collector {
 		spotInstanceRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_requests"),
 			"Spot instance requests count.",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceAzGroupConstraintRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_az_group_contraint_requests"),
 			"Spot instance requests with az-group-constraint status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceBadParametersRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_bad_parameters_requests"),
 			"Spot instance requests with az-group-constraint status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceCanceledBeforeFulfillmentRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_canceled_before_fulfillment_requests"),
 			"Spot instance requests with canceled-before-fulfillment status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceCapacityNotAvailableRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_capacity_not_available_requests"),
 			"Spot instance requests with capacity-not-available status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceConstraintNotFulfillableRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_constraint_not_fulfillable_requests"),
 			"Spot instance requests with constraint-not-fulfillable status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceFulfilledRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_fulfilled_requests"),
 			"Spot instance requests with fulfilled status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceStoppedByPriceRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_stopped_by_price_requests"),
 			"Spot instance requests with instance-stopped-by-price status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceStoppedByUserRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_stopped_by_user_requests"),
 			"Spot instance requests with instance-stopped-by-user status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceStoppedNoCapacityRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_stopped_no_capacity_requests"),
 			"Spot instance requests with instance-stopped-no-capacity status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceTerminatedByPriceRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_terminated_by_price_requests"),
 			"Spot instance requests with instance-terminated-by-price status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceTerminatedByScheduleRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_terminated_by_schedule_requests"),
 			"Spot instance requests with instance-terminated-by-schedule status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceTerminatedByServiceRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_terminated_by_service_requests"),
 			"Spot instance requests with instance-terminated-by-service status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceTerminatedByUserRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_terminated_by_user_requests"),
 			"Spot instance requests with instance-terminated-by-user status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceTerminatedLaunchGroupConstraintRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_terminated_launch_group_constraint_requests"),
 			"Spot instance requests with instance-terminated-launch-group-constraint status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceTerminatedNoCapacityRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_terminated_no_capacity_requests"),
 			"Spot instance requests with instance-terminated-no-capacity status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceLaunchGroupConstraintRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_launch_group_constraint_requests"),
 			"Spot instance requests with launch-group-constraint status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceMarkedForStopRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_marked_for_stop_requests"),
 			"Spot instance requests with marked-for-stop status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceMarkedForTerminationRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_marked_for_termination_requests"),
 			"Spot instance requests with marked-for-termination status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceNotScheduledYetRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_not_scheduled_yet_requests"),
 			"Spot instance requests with not-scheduled-yet status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstancePendingEvaluationRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_pending_evaluation_requests"),
 			"Spot instance requests with pending-evaluation status count.",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstancePendingFulfillmentRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_pending_fulfillment_requests"),
 			"Spot instance requests with pending-fulfillment status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstancePlacementGroupConstraintRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_placement_group_constraint_requests"),
 			"Spot instance requests with placement-group-constraint status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstancePriceTooLowRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_price_too_low_requests"),
 			"Spot instance requests with price-too-low status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceRequestCanceledAndInstanceRunningRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_request_canceled_and_instance_running_requests"),
 			"Spot instance requests with request-canceled-and-instance-running status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceScheduleExpiredRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_schedule_expired_requests"),
 			"Spot instance requests with schedule_expired status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 		spotInstanceSystemErrorRequests: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "instance_system_error_requests"),
 			"Spot instance requests with system-error status count",
-			[]string{},
+			[]string{"region"},
 			nil,
 		),
 	}
@@ -363,140 +364,169 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 		}
 	}
 
+	region := os.Getenv("AWS_REGION")
+
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceRequests,
 		prometheus.GaugeValue,
 		float64(len(result.SpotInstanceRequests)),
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceAzGroupConstraintRequests,
 		prometheus.GaugeValue,
 		azGroupConstraintRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceBadParametersRequests,
 		prometheus.GaugeValue,
 		badParametersRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceCanceledBeforeFulfillmentRequests,
 		prometheus.GaugeValue,
 		canceledBeforeFulfillmentRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceCapacityNotAvailableRequests,
 		prometheus.GaugeValue,
 		capacityNotAvailableRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceConstraintNotFulfillableRequests,
 		prometheus.GaugeValue,
 		constraintNotFulfillableRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceFulfilledRequests,
 		prometheus.GaugeValue,
 		fulfilledRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceStoppedByPriceRequests,
 		prometheus.GaugeValue,
 		instanceStoppedByPriceRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceStoppedByUserRequests,
 		prometheus.GaugeValue,
 		instanceStoppedByUserRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceStoppedNoCapacityRequests,
 		prometheus.GaugeValue,
 		instanceStoppedNoCapacityRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceTerminatedByPriceRequests,
 		prometheus.GaugeValue,
 		instanceTerminatedByPriceRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceTerminatedByScheduleRequests,
 		prometheus.GaugeValue,
 		instanceTerminatedByScheduleRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceTerminatedByServiceRequests,
 		prometheus.GaugeValue,
 		instanceTerminatedByServiceRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceTerminatedByUserRequests,
 		prometheus.GaugeValue,
 		instanceTerminatedByUserRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceTerminatedLaunchGroupConstraintRequests,
 		prometheus.GaugeValue,
 		instanceTerminatedLaunchGroupConstraintRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceTerminatedNoCapacityRequests,
 		prometheus.GaugeValue,
 		instanceTerminatedNoCapacityRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceLaunchGroupConstraintRequests,
 		prometheus.GaugeValue,
 		launchGroupConstraintRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceMarkedForStopRequests,
 		prometheus.GaugeValue,
 		markedForStopRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceMarkedForTerminationRequests,
 		prometheus.GaugeValue,
 		markedForTerminationRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceNotScheduledYetRequests,
 		prometheus.GaugeValue,
 		notScheduledYetRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstancePendingEvaluationRequests,
 		prometheus.GaugeValue,
 		pendingEvaluationRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstancePendingFulfillmentRequests,
 		prometheus.GaugeValue,
 		pendingFulfillmentRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstancePlacementGroupConstraintRequests,
 		prometheus.GaugeValue,
 		placementGroupConstraintRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstancePriceTooLowRequests,
 		prometheus.GaugeValue,
 		priceTooLowRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceRequestCanceledAndInstanceRunningRequests,
 		prometheus.GaugeValue,
 		requestCanceledAndInstanceRunningRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceScheduleExpiredRequests,
 		prometheus.GaugeValue,
 		scheduleExpiredRequests,
+		region,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		c.spotInstanceSystemErrorRequests,
 		prometheus.GaugeValue,
 		systemErrorRequests,
+		region,
 	)
 }
 
